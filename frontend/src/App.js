d3a009326243fbd8e3c39e32329c770b983546d6
MolemoMM <mamashelamolemo@gmail.com>
2025-07-02 11:28:58 +0200

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,10 +14,9 @@ import BookTicket from "./components/TicketBooking/BookTicket";
 import RouteList from "./components/RouteManagement/RouteList";
 import ViewSchedules from "./components/User/ViewSchedules";
 
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import { DataProvider } from "./context/DataContext";
 import PrivateRoute from "./utils/PrivateRoute";
-import { useAuth } from "./context/AuthContext";
 
 // Component to handle root route redirection
 const RootRedirect = () => {
@@ -32,80 +31,87 @@ const RootRedirect = () => {
   return <Navigate to="/login" replace />;
 };
 
+// Main App Routes component that has access to auth context
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* Root redirect - goes to login if not authenticated */}
+      <Route path="/" element={<RootRedirect />} />
+      
+      {/* Auth Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Admin Dashboard Routes */}
+      <Route
+        path="/admin/*"
+        element={
+          <PrivateRoute allowedRoles={["ADMIN"]}>
+            <AdminDashboard />
+          </PrivateRoute>
+        }
+      />
+
+      {/* User Routes */}
+      <Route
+        path="/user"
+        element={
+          <PrivateRoute allowedRoles={["USER"]}>
+            <UserDashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/user/tickets"
+        element={
+          <PrivateRoute allowedRoles={["USER"]}>
+            <TicketList />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/user/book-ticket"
+        element={
+          <PrivateRoute allowedRoles={["USER"]}>
+            <BookTicket />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/user/routes"
+        element={
+          <PrivateRoute allowedRoles={["USER"]}>
+            <RouteList />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/user/routes/:routeId/schedules"
+        element={
+          <PrivateRoute allowedRoles={["USER"]}>
+            <ViewSchedules />
+          </PrivateRoute>
+        }
+      />
+
+      {/* Public Routes */}
+      <Route path="/schedules" element={<BusScheduleList />} />
+      <Route path="/schedules/add" element={<AddSchedule />} />
+      <Route path="/routes" element={<RouteList />} />
+
+      {/* Default Redirect - redirect to login */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+};
+
 function App() {
   return (
     <Router>
       <AuthProvider>
         <DataProvider>
           <div className="App">
-            <Routes>
-              {/* Root redirect - goes to login if not authenticated */}
-              <Route path="/" element={<RootRedirect />} />
-              
-              {/* Auth Routes */}
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-
-              {/* Admin Dashboard Routes */}
-              <Route
-                path="/admin/*"
-                element={
-                  <PrivateRoute allowedRoles={["ADMIN"]}>
-                    <AdminDashboard />
-                  </PrivateRoute>
-                }
-              />
-
-              {/* User Routes */}
-              <Route
-                path="/user"
-                element={
-                  <PrivateRoute allowedRoles={["USER"]}>
-                    <UserDashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user/tickets"
-                element={
-                  <PrivateRoute allowedRoles={["USER"]}>
-                    <TicketList />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user/book-ticket"
-                element={
-                  <PrivateRoute allowedRoles={["USER"]}>
-                    <BookTicket />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user/routes"
-                element={
-                  <PrivateRoute allowedRoles={["USER"]}>
-                    <RouteList />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user/routes/:routeId/schedules"
-                element={
-                  <PrivateRoute allowedRoles={["USER"]}>
-                    <ViewSchedules />
-                  </PrivateRoute>
-                }
-              />
-
-              {/* Public Routes */}
-              <Route path="/schedules" element={<BusScheduleList />} />
-              <Route path="/schedules/add" element={<AddSchedule />} />
-              <Route path="/routes" element={<RouteList />} />
-
-              {/* Default Redirect - redirect to login */}
-              <Route path="*" element={<Navigate to="/login" replace />} />
-            </Routes>
+            <AppRoutes />
           </div>
           <ToastContainer position="top-right" autoClose={3000} />
         </DataProvider>
---
+++ frontend/src/components/Admin/AdminDashboard_backup.js
@@ -0,0 +1,1 @@
+// Backup of old AdminDashboard.js before replacement
